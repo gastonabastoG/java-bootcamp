7d59d8680ca06232d2bb3d70a37238d139e0441c
Mariano Fidececchi <mfidececchi@yahoo.com>
2015-09-17 11:11:18 -0300

    Topic 2 Fixes1
    - It was changed the visibility of the constructor method respect Singleton pattern.
    - The war file was renamed in pom.xml file and it was rebuilt by "clean install" to Exercise4_5.war.

--- Practice/Topic2/Exercise1/src/main/java/mfidececchi/services/TextService.java
+++ Practice/Topic2/Exercise1/src/main/java/mfidececchi/services/TextService.java
@@ -5,11 +5,11 @@ public class TextService {
 	private static TextService ts;
 	private String text = "This is a text provided by TextService";
 
-	public TextService() {
-		
+	private TextService() {
+
 	}
-	
-	public static TextService getInstance() {
+
+	public TextService getInstance() {
 		if (ts == null) {
 			ts = new TextService();
 		}
@@ -17,7 +17,7 @@ public class TextService {
 	}
 
 	public String getText() {
-		return text;		
+		return text;
 	}
 
 }
